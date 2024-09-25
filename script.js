0d9c88e89558967eb60320e5d4e1f0b52581f5e9
¨Fabianavi¨ <vivasfabi2007@gmail.com>
2024-09-25 17:02:14 -0400

    index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+let persona = {
+    name: 'Devian',
+    lastname: 'Mesias',
+    age: '27',
+    hair: '#000',
+    adress: {
+       city: 'tucupita',
+       country: 'Mexico',
+       street: 'main street',
+    }
+}
+
+let cancion{
+    id:
+    title: Flowers
+    author: Miley Cyrus
+}
